6a0866a90ed7da7a8a3421c1f46756595bc5111c
hashMi <854059946@qq.com>
2023-08-01 16:30:07 +0800

    feat: 模版搭建完成:zap:

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Why so serious my dear 854059946@qq.com
  * @Date: 2023-07-18 23:13:30
- * @LastEditors: Why so serious my dear 854059946@qq.com
- * @LastEditTime: 2023-07-18 23:14:18
+ * @LastEditors: hashMi 854059946@qq.com
+ * @LastEditTime: 2023-08-01 09:37:10
  * @FilePath: /my-vite-project/scripts/preinstall.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
